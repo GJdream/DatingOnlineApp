12d7d4852edbe8d5ac397aa5bcb1d602c864a6a0
minhduccm <myhostdotnet@gmail.com>
2014-05-09 18:11:00 +0700

    updating from company

--- src/com/example/datingonline/HomeActivity.java
+++ src/com/example/datingonline/HomeActivity.java
@@ -1,21 +1,36 @@
 package com.example.datingonline;
 
+import Fragment.HomeFragment;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class HomeActivity extends Activity implements OnClickListener  {
+public class HomeActivity extends FragmentActivity implements OnClickListener  {
 	public final static String 	EXTRA_USERNAME = "com.example.datingonline.USERNAME";
+	private HomeFragment homeFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+//		if (savedInstanceState == null) {
+//	        // Add the fragment on initial activity setup
+//			homeFragment = new HomeFragment();
+//	        getSupportFragmentManager()
+//	        .beginTransaction()
+//	        .add(R., homeFragment)
+//	        .commit();
+//	    } else {
+//	        // Or set the fragment from restored state info
+//	    	homeFragment = (HomeFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
+//	    }
 		
 		Button loginBtn = (Button) findViewById(R.id.loginButton);
 		loginBtn.setOnClickListener(this);
